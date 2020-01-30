b489ab0650564460c7dfebabb0d41bf64e5b087f
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-01-30 23:56:35 +0300

    Add Methods

---
+++ ApplicationLifeCycle/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ApplicationLifeCycle.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Жизненный цикл приложения</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        Для просмотра результата работы:
+        <ol>
+            <li>
+                
+            </li>
+            <li>
+
+            </li>
+            <li>
+
+            </li>
+        </ol>
+        </div>
+    </form>
+</body>
+</html>
--- ApplicationLifeCycle/Global.asax.cs
+++ ApplicationLifeCycle/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
@@ -11,6 +12,66 @@ namespace ApplicationLifeCycle
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+            Debug.WriteLine("Start");
         }
+        protected void Application_End(object sender, EventArgs e)
+        {
+            Debug.WriteLine("End");
+        }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Error");
+            Exception ex = Server.GetLastError().GetBaseException();
+        }
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Session Start");
+        }
+        protected void Session_End(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Session End");
+        }
+        /////////////////////////////////////////
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Begin Request");
+        }
+        protected void Application_AutantificateRequest(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Autantificate Request");
+        }
+        protected void Application_AutorizeRequest(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Autorize Request");
+        }
+        protected void Application_ResolveRequestCache(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Resolve Request Cache");
+        }
+        protected void Application_AcquireRequestSate(object sender, EventArgs e)
+        {
+            Debug.WriteLine("AcquireRequestState");
+        }
+        protected void Application_PreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            Debug.WriteLine("PreRequestHandlerExecute");
+        }
+        protected void Application_PostRequestHandlerExecute(object sender, EventArgs e)
+        {
+            Debug.WriteLine("PostRequestHandlerExecute");
+        }
+        protected void Application_ReleaseRequestState(object sender, EventArgs e)
+        {
+            Debug.WriteLine("ReleaseReqquestState");
+        }
+        protected void Application_UpdaeteRequestCache(object sender, EventArgs e)
+        {
+            Debug.WriteLine("UpdateRequestCache");
+        }
+        protected void Application_EndRequst(object sender, EventArgs e)
+        {
+            Debug.WriteLine("EndRequest");
+        }
+        
     }
 }
